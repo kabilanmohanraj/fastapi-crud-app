e1352bfdb7aa7f3204aba6e79095a8485d223ce5
Kabilan Mohanraj <kabilanmohanraj@gmail.com>
2025-01-17 12:09:11 -0500

    create initial project structure

---
+++ backend/app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from app.api.routes import login, books, users, events
+from app.core.db import init_db
+
+def create_application() -> FastAPI:
+    application = FastAPI(title="Library Management Application")
+    application.include_router(login.router)
+    application.include_router(books.router)
+    # application.include_router(users.router)
+    application.include_router(events.router)
+    return application
+
+library_app = create_application()
+
+@library_app.on_event("startup")
+def on_startup():
+    init_db()
+
+@library_app.get("/")
+async def root():
+    return {"message": "Welcome to the Library Management Application!"}
