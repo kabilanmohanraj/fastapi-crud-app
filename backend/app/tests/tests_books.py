d499010b312c8fdcf6b68c9f7224b94db37a7f75
Kabilan Mohanraj <kabilanmohanraj@gmail.com>
2025-01-19 18:49:07 -0500

    update tests for the book endpoint

--- backend/app/tests/tests_books.py
+++ backend/app/tests/tests_books.py
@@ -4,6 +4,10 @@ from backend.app.main import library_app
 
 client = TestClient(library_app)
 
+# the instance of the book_id used to test the CRUD operations
+# we create a book in the DB then use it to test the CRUD operations
+book_id = -1
+
 def authenticate():
     form_data = {
         "username": "root@test.com",
@@ -12,25 +16,6 @@ def authenticate():
     response = client.post("/login", data=form_data)
     assert response.status_code == 200
     return response.json()["access_token"]
-
-def test_get_books():
-    access_token = authenticate()
-    headers = {"Authorization": f"Bearer {access_token}"}
-    response = client.get("/books?skip=0&limit=10", headers=headers)
-    assert response.status_code == 200
-    assert isinstance(response.json(), list)
-
-def test_get_book_by_id():
-    access_token = authenticate()
-    headers = {"Authorization": f"Bearer {access_token}"}
-    book_id = 1  # Assuming book with ID 1 exists
-    response = client.get(f"/books/{book_id}", headers=headers)
-    if response.status_code == 200:
-        assert "title" in response.json()
-        assert "author" in response.json()
-    else:
-        assert response.status_code == 404
-        assert response.json()["detail"] == "Book not found"
 
 def test_create_book():
     access_token = authenticate()
@@ -43,13 +28,31 @@ def test_create_book():
         "genre": "Fiction"
     }
     response = client.post("/books/", json=book_data, headers=headers)
+    book_id = response.json()["id"]
     assert response.status_code == 200
     assert response.json()["title"] == book_data["title"]
+
+def test_get_books():
+    access_token = authenticate()
+    headers = {"Authorization": f"Bearer {access_token}"}
+    response = client.get("/books?skip=0&limit=10", headers=headers)
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+def test_get_book_by_id():
+    access_token = authenticate()
+    headers = {"Authorization": f"Bearer {access_token}"}
+    response = client.get(f"/books/{book_id}", headers=headers)
+    if response.status_code == 200:
+        assert "title" in response.json()
+        assert "author" in response.json()
+    else:
+        assert response.status_code == 404
+        assert response.json()["detail"] == "Book not found."
 
 def test_update_book():
     access_token = authenticate()
     headers = {"Authorization": f"Bearer {access_token}"}
-    book_id = 501  # Assuming book with ID 1 exists
     update_data = {
         "title": "Updated Book Title"
     }
@@ -58,16 +61,15 @@ def test_update_book():
         assert response.json()["title"] == update_data["title"]
     else:
         assert response.status_code == 404
-        assert response.json()["detail"] == "Book not found"
+        assert response.json()["detail"] == "Book not found."
 
 def test_delete_book():
     access_token = authenticate()
     headers = {"Authorization": f"Bearer {access_token}"}
-    book_id = 501  # Assuming book with ID 1 exists
     response = client.delete(f"/books/{book_id}", headers=headers)
     if response.status_code == 200:
         assert "title" in response.json()
         assert "author" in response.json()
     else:
         assert response.status_code == 404
-        assert response.json()["detail"] == "Book not found"
+        assert response.json()["detail"] == "Book not found."
